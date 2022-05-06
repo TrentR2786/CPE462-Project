055e7f6c7a2b83fba142982f8fa51e23b01ff45f
Trent Reichenbach <treichen@stevens.edu>
2022-05-06 16:52:46 -0400

    Cited sources for image reading code

--- project.cpp
+++ project.cpp
@@ -5,10 +5,15 @@ using namespace std;
 using namespace cv;
 
 int main(int argc, char* argv[]) {
+
+// Code sources: https://www.opencv-srf.com/2017/11/load-and-display-image.html, imgproc.cpp (Class)
   Mat image = imread(argv[1]);
 
   if (image.empty()) {
     cout << "Could not open or find the image" << endl;
     return -1;
   }
+// ---------------------------------------------------
+
+
 }
