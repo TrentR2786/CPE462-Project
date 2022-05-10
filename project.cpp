b17624078a297dc8403af666c7c75167f1f83f4c
Trent Reichenbach <treichen@stevens.edu>
2022-05-09 22:21:01 -0400

    Added headers for Tesseract

--- project.cpp
+++ project.cpp
@@ -1,13 +1,18 @@
+#include <leptronica/allheaders.h>
+#include <tesseract/baseapi.h>
+
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <string>
 
 using namespace std;
 using namespace cv;
 
 int main(int argc, char* argv[]) {
-  // Code sources:
-  // https://www.opencv-srf.com/2017/11/load-and-display-image.html, imgproc.cpp
-  // (Class)
+  /*****************************************
+  Code sources:
+  https://www.opencv-srf.com/2017/11/load-and-display-image.html, imgproc.cpp */
+
   Mat image = imread(argv[1]);
 
   if (image.empty()) {
@@ -21,5 +26,6 @@ int main(int argc, char* argv[]) {
 
   Mat_<uchar> image_gray(image.rows, image.cols);
   cvtColor(image, image_gray, COLOR_BGR2GRAY);
-  // ---------------------------------------------------
+
+  /*******************************************/
 }
