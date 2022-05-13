c65e0daa1add8bb07d3d94334fca3c7911bd5eb5
muradarslaner <marslan1@stevens.edu>
2022-05-13 14:06:50 -0400

    censor test

--- project.cpp
+++ project.cpp
@@ -9,18 +9,12 @@ using namespace std;
 using namespace cv;
 
 int main(int argc, char* argv[]) {
-  /*****************************************/
-  // Code sources:
-  // https://www.opencv-srf.com/2017/11/load-and-display-image.html, imgproc.cpp
-
+  //read image
   Mat image = imread(argv[1]);
-
   if (image.empty()) {
-    cout << "Could not open or find the image" << endl;
-    return -1;
+    cout << "Invalid image" << endl;
+    return 1;
   }
-
-  /*****************************************/
 
   //grayscale
   Mat image_gray;
@@ -32,38 +26,42 @@ int main(int argc, char* argv[]) {
 
   //threshold
   Mat image_thresh;
-  threshold(image_blur, image_thresh, 0, 255, THRESH_BINARY + THRESH_OTSU);
+  threshold(image_blur, image_thresh, 0, 255, THRESH_BINARY_INV + THRESH_OTSU);
   
   //find contours
   vector<vector<Point>> contours;
   vector<Vec4i> hierarchy;
-  findContours(image_thresh, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE);
-
-  //draw contours in green
-  Mat image_contours;
-  cvtColor(image_thresh, image_contours, COLOR_GRAY2BGR);
-  Mat image_rect = image_contours.clone();
-
-  for (size_t i = 0; i < contours.size(); i++) {
-    drawContours(image_contours, contours, i, Scalar(0,255,0));
-  }
+  findContours(image_thresh, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
 
   //find+draw rectangles in red
+  Mat image_rect;
+  cvtColor(image_thresh, image_rect, COLOR_GRAY2BGR);
   vector<Rect> boundRect(contours.size());
+
   for (size_t i = 0; i < contours.size(); i++) {
     boundRect[i] = boundingRect(contours[i]);
     rectangle(image_rect, boundRect[i].tl(), boundRect[i].br(), Scalar(0,0,255));
   }
 
-  //display
+  //display rects
   imshow("Rect Image", image_rect);
   waitKey(0);
   destroyWindow("Rect Image");
 
+  //draw black boxes on contours (test for censoring)
+  Mat image_censored = image.clone();
+  for (size_t i = 0; i < contours.size(); i++) {
+    rectangle(image_censored, boundRect[i].tl(), boundRect[i].br(), Scalar(0,0,0), FILLED);
+  }
+
+  //display censor
+  imshow("Censored Image", image_censored);
+  waitKey(0);
+  destroyWindow("Censored Image");
+
   /*****************************************/
   // Code source:
   // https://medium.com/building-a-simple-text-correction-tool/basic-ocr-with-tesseract-and-opencv-34fae6ab3400
-
   tesseract::TessBaseAPI* ocr = new tesseract::TessBaseAPI();
   ocr->Init(NULL, "eng", tesseract::OEM_LSTM_ONLY);
   ocr->SetPageSegMode(tesseract::PSM_AUTO);
@@ -72,6 +70,6 @@ int main(int argc, char* argv[]) {
   string text = string(ocr->GetUTF8Text());
   cout << text;
   ocr->End();
-  
+  return 0;
   /*****************************************/
 }
